b66950d588f7dd60792d090915efc7d208d93694
Shashwat Kapoor <shashwatquora2000@gmail.com>
2023-08-22 10:48:47 +0530

    my first react project

---
+++ src/MovieList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+function MovieList(props){
+
+    const {movies, onIncStars, onDecStars, onClickFav, onClickAddtocart} =  props;
+        
+    return(
+        <div className="main">
+            {movies.map((movie,index) => (
+            <MovieCard movies = {movie}
+                        key = {index}
+                        onIncStars = {onIncStars}
+                        onDecStars = {onDecStars}
+                        onClickFav = {onClickFav}
+                        onClickAddtocart = {onClickAddtocart}/>
+
+        ))}  
+
+
+         {/* so here very important concept is come into picture which is prop drilling              */}
+        </div>
+    )
+}
+
+
+export default MovieList;
